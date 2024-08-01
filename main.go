982ca8afa2aa8c5538067e49c6e346dd2a942ee8
ArenAlrick <arenalrick@gmail.com>
2024-08-01 09:52:14 -0400

    Messing around.  dunno.

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import . "fmt"
 
 func main () {
 
+	Println("Hello world!  This is my first Go program!")
+
+
+	var a int = 5
+	var b float32 = 4.32
+	const pi float64 = 3.1415139
+
+	var (
+		c, d, e, f = 8, "Hello", 14, 15
+	)
+
+	Println(a)
+	Println(b)
+	Println(c)
+	Println(d)
+	Println(e)
+	Println(f)
 
 }
